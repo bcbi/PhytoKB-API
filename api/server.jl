1aa61487ac47fe9e5428ef1f340cfe73fa032545
fgelin <fernando_gelin@brown.edu>
2018-05-18 09:14:27 -0400

    adds server.jl, sets up path neighbors to work with api

---
+++ api/server.jl
@@ -0,0 +1,32 @@
+using HTTP
+include("../src/path_neighbors.jl")
+
+
+
+function run_server()
+
+    query_dict = Dict()
+
+    HTTP.listen() do request::HTTP.Request
+
+        println("******************")
+        @show request
+        println("******************")
+
+        uri = parse(HTTP.URI, request.target)
+        query_dict = HTTP.queryparams(uri)
+
+        headers = Dict{AbstractString,AbstractString}(
+            "Server"            => "Julia/$VERSION",
+            "Content-Type"      => "text/html; charset=utf-8",
+            "Content-Language"  => "en",
+            "Date"              => Dates.format(now(Dates.UTC), Dates.RFC1123Format),
+            "Access-Control-Allow-Origin" => "*" )
+
+        return HTTP.Response(200, HTTP.Headers(collect(headers)), body = main(query_dict["id"], query_dict["level"], length_dict, height_dict, dname_dict, dict_path))
+    end
+
+
+end
+
+run_server()
--- src/path_neighbors.jl
+++ src/path_neighbors.jl
@@ -2,26 +2,29 @@
 # May 05, 2018
 
 
-function get_names(leaf_arr, dict_path, length_dict, dname_dict)
-    leaf_nam = Any[]
-    for i=1:length(leaf_arr)
+function get_names(leaf_arr::Array, dict_path::Dict, length_dict::Dict, dname_dict::Dict)
+    num_leaves = length(leaf_arr)
+    leaf_nam = Array{String}(num_leaves)
+    for i=1:num_leaves
         leaf = "$(dname_dict[dict_path[leaf_arr[i]]])\:$(length_dict[leaf_arr[i]])"
-        push!(leaf_nam, leaf)
+        leaf_nam[i] = leaf
     end
     return leaf_nam
 end
 
-function get_neighbors(input_txn, input_lvl, path_dict)
+function get_neighbors(input_txn::String, input_lvl::Int64, path_dict::Dict)
     input_txn_path = path_dict[input_txn]
-    input_txn_path = split(input_txn_path, "\.")
+    input_txn_path_arr = split(input_txn_path, "\.")
     #println(input_txn_path[1:end-(2*input_lvl)])
     #println(input_txn_path)
-    input_txn_parent = join(input_txn_path[1:end-(2*input_lvl-1)], "\.")
+    input_txn_parent = join(input_txn_path_arr[1:end-(2*input_lvl-1)], "\.")
+    #=
     if input_txn_parent == "" || input_txn_parent == "0"
         println("Reached maximum depth")
     else
         println(input_txn_parent)
     end
+    =#
     #println(ismatch(Regex("$input_txn_parent\.[0-9]+"), "0.1.3.2.2.3.2"))
     path_arr = collect(values(path_dict))
     neighbor = filter(x -> ismatch(Regex("$input_txn_parent.[0-9]+"), x), path_arr)
@@ -41,28 +44,25 @@ function create_newick_qry(idx_arr, path, lvl, olvl, str)
         #println("\=\=\>$path")
         str = "$str\,$path"
         #println(str)
-        j = 1
         for i in 1:len
             #println("$path\.$i")
             str = create_newick_qry(idx_arr, "$path\.$i", lvl, olvl, str)
-            j += 1
         end
     end
     return str
 end
 
 
-function create_newick(nbr_arr, qry_arr)
+function create_newick(nbr_arr, qry_arr, slvl, dict_path, length_dict, dname_dict)
     qstr = qry_arr[findfirst(map(x->length(split(x, "\.")), qry_arr), maximum(map(x->length(split(x, "\.")), qry_arr)))]
     qstr_len = length(split(qstr, "\."))
 
     nwk = ""
-    j = qstr_len
     i = parse(Int64, split(qstr, "\.")[end])
     prev_i = i
     while i >= slvl
-        #println(nwk)
-        #println("$i \=\> $prev_i \=\> $slvl \=\> $qstr")
+        println(nwk)
+        println("$i \=\> $prev_i \=\> $slvl \=\> $qstr")
         leaf = filter(x -> ismatch(Regex("$qstr"), x), nbr_arr)
         leaf = get_names(leaf, dict_path, length_dict, dname_dict)
         #println(leaf)
@@ -101,7 +101,6 @@ function create_newick(nbr_arr, qry_arr)
         end
         nbr_arr = filter(x -> !(ismatch(Regex("$qstr"), x)), nbr_arr)
         qry_arr = filter(x -> !(ismatch(Regex("$qstr"), x)), qry_arr)
-        j = j - 2
         if length(qry_arr) > 0
             qstr = qry_arr[findfirst(map(x->length(split(x, "\.")), qry_arr), maximum(map(x->length(split(x, "\.")), qry_arr)))]
         else
@@ -119,7 +118,8 @@ end
 # Load data from tree file stored as taxon and their respective paths #
 #######################################################################
 
-lengthFile = open("hd_tree_taxon_length.txt")
+
+lengthFile = open("src/hd_tree_taxon_length.txt")
 length_dict = Dict{Any, Any}()
 for ln in eachline(lengthFile)
     ln = chomp(ln)
@@ -128,7 +128,7 @@ for ln in eachline(lengthFile)
     length_dict[path] = len
 end
 
-heightFile = open("hd_tree_taxon_height.txt")
+heightFile = open("src/hd_tree_taxon_height.txt")
 height_dict = Dict{Any, Any}()
 for ln in eachline(heightFile)
     ln = chomp(ln)
@@ -137,7 +137,7 @@ for ln in eachline(heightFile)
     height_dict[path] = hgt
 end
 
-pathFile = open("hd_tree_taxon_path.txt")
+pathFile = open("src/hd_tree_taxon_path.txt")
 
 path_dict = Dict{Any, Any}()
 dict_path  = Dict{Any, Any}()
@@ -149,7 +149,7 @@ for ln in eachline(pathFile)
     dict_path[path] = txn
 end
 
-drugFile = open("drugid_names.txt")
+drugFile = open("src/drugid_names.txt")
 
 dname_dict  = Dict{Any, Any}()
 for ln in eachline(drugFile)
@@ -160,43 +160,49 @@ for ln in eachline(drugFile)
     dname_dict[txn] = name
 end
 
-#nwk_str = "(mammal:0.14,turtle:0.02,(rayfinfish:0.25,(frog:0.01,salamander:0.01,crow:0.02)50:0.12)95:0.09);"
 
-#########################################
-# Input from user => drug/herb ID       #
-# Input from user => How many levels up #
-#########################################
+function main(input_txn::String, input_lvl::String, length_dict, height_dict, dname_dict, dict_path)
 
-input_txn = ARGS[1]
-input_lvl = parse(Int64, ARGS[2])
+    #nwk_str = "(mammal:0.14,turtle:0.02,(rayfinfish:0.25,(frog:0.01,salamander:0.01,crow:0.02)50:0.12)95:0.09);"
 
-nbr_arr, parent_path = get_neighbors(input_txn, input_lvl, path_dict)
+    #########################################
+    # Input from user => drug/herb ID       #
+    # Input from user => How many levels up #
+    #########################################
 
-str = nbr_arr[findfirst(map(x->length(split(x, "\.")), nbr_arr), maximum(map(x->length(split(x, "\.")), nbr_arr)))]
-#println("str\:$str")
-max_lvl = parse(Int64, split(str, "\.")[end-1])
-inp_st = length(split(parent_path, "\.")) + 1
-#println(max_lvl)
-#println(inp_st)
-idx_arr = Any[]
-for i in inp_st:2:length(split(str, "\."))
-    re = repeat("\\.\[0\-9\]\+\\.\[0\-9\]\+", Int64((i-1)/2))
-    leaf = filter(x -> ismatch(Regex("0$re"), x), nbr_arr)
-    idx = maximum(map(x->parse(Int64, split(x, "\.")[i]), leaf))
-    push!(idx_arr, idx)
+    #input_txn = ARGS[1]
+    #input_lvl = parse(Int64, ARGS[2])
+    input_lvl = parse(Int64, input_lvl)
+
+    nbr_arr, parent_path = get_neighbors(input_txn, input_lvl, path_dict)
+
+    str = nbr_arr[findfirst(map(x->length(split(x, "\.")), nbr_arr), maximum(map(x->length(split(x, "\.")), nbr_arr)))]
+    #println("str\:$str")
+    max_lvl = parse(Int64, split(str, "\.")[end-1])
+    inp_st = length(split(parent_path, "\.")) + 1
+    #println(max_lvl)
+    #println(inp_st)
+    idx_arr = Any[]
+    for i in inp_st:2:length(split(str, "\."))
+        re = repeat("\\.\[0\-9\]\+\\.\[0\-9\]\+", Int64((i-1)/2))
+        leaf = filter(x -> ismatch(Regex("0$re"), x), nbr_arr)
+        idx = maximum(map(x->parse(Int64, split(x, "\.")[i]), leaf))
+        push!(idx_arr, idx)
+    end
+
+    println(idx_arr)
+
+    path = join(split(parent_path, "\.")[1:end-1], "\.")
+    #println("path\:$path")
+    #println("max_lvl\:$max_lvl")
+    path != "0" ? olvl = parse(Int64, split(path, "\.")[end-1]) : olvl = 0
+    slvl = parse(Int64, split(parent_path, "\.")[end])
+    qry_str = create_newick_qry(idx_arr, path, 0, olvl, "")
+    qry_str = qry_str[2:end]
+    #println(qry_str)
+    qry_arr = split(qry_str, "\,")
+
+    nwk_str = create_newick(nbr_arr, qry_arr, slvl, dict_path, length_dict, dname_dict)
+    #println(nwk_str)
+    return nwk_str
 end
-
-#println(idx_arr)
-
-path = join(split(parent_path, "\.")[1:end-1], "\.")
-#println("path\:$path")
-#println("max_lvl\:$max_lvl")
-path != "0" ? olvl = parse(Int64, split(path, "\.")[end-1]) : olvl = 0
-slvl = parse(Int64, split(parent_path, "\.")[end])
-qry_str = create_newick_qry(idx_arr, path, 0, olvl, "")
-qry_str = qry_str[2:end]
-#println(qry_str)
-qry_arr = split(qry_str, "\,")
-
-nwk_str = create_newick(nbr_arr, qry_arr)
-println(nwk_str)
